c048b43051598f238ccf9fb315a9944028282618
DHUYUKSHAA <dhuyukshaarunkumar@gmail.com>
2026-08-31 12:11:24 +0530

    Initial commit

---
+++ demo.js
@@ -0,0 +1,43 @@
+//  var name = "Dhuyu"
+//  var age = 20
+//  var dep = "ECE"
+//  function details(){
+//     console.log("Name: "+name);
+//     console.log("Age: "+age);
+//     console.log("dep: "+dep);
+//  }
+//  details()
+
+
+// var a = 10
+// var b = 20
+// function add(a,b){
+//     return a+b;
+// }
+// var result = add(10,40);
+// console.log(result);
+
+// var act = "Kamal"
+// var Player = "Dhoni"
+// var Movie = "Anbe Sivam"
+// function fav(){
+//     console.log("Actor: "+act);
+//     console.log("Player: "+Player);
+//     console.log("Movie: "+Movie);
+// }
+// fav()
+
+
+// function isVowel(ch){
+//     if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// var res=isVowel('t');
+// if(res) console.log("Vowel");
+// else console.log("Consonant");
+
+
